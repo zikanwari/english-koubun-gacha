89b9254f497939b2a67cee048eae4524eceb9352
itou-daiki <141300506+itou-daiki@users.noreply.github.com>
2023-09-27 22:49:32 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,18 @@ if st.button('ガチャを引く！'):
     subset_df = words_df[words_df['レア度'] == chosen_rarity]
     selected_word = subset_df.sample().iloc[0]
     
-    st.header(f"単語名: {selected_word['単語']}")
-    st.subheader(f"レア度: {selected_word['レア度']}")
+    # セッションステートに選択された単語を保存
+    st.session_state.selected_word = selected_word
+    st.session_state.display_meaning = False
+
+st.header(f"単 of_completion 士のもの！")
+if 'selected_word' in st.session_state:
+    st.header(f"単語名: {st.session_state.selected_word['単語']}")
+    st.subheader(f"レア度: {st.session_state.selected_word['レア度']}")
 
     # 意味を確認するボタンを追加
     if st.button('意味を確認する'):
-        st.write(f"意味: {selected_word['意味']}")
+        st.session_state.display_meaning = True
+
+    if st.session_state.display_meaning:
+        st.write(f"意味: {st.session_state.selected_word['意味']}")
