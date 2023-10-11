1f48c59144346846fad4e1e2e9e2c56b7825bd9e
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-10-11 23:35:46 +0000

    助動詞化

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,32 +2,24 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="古文単語ガチャ")
+st.set_page_config(page_title="助動詞ガチャ")
 
 # タイトルと説明
-st.title('古文単語ガチャ')
+st.title('助動詞ガチャ')
 
-st.write('古文単語をランダムに表示して、勉強をサポートします！')
+st.write('助動詞をランダムに表示して、勉強をサポートします！')
 st.write('がんばってください！')
 
 # Load the data
 @st.cache
 def load_data():
-    return pd.read_excel("古文単語リスト.xlsx")
+    return pd.read_excel("助動詞.xlsx")
 
 words_df = load_data()
 
 # ガチャ機能
 if st.button('ガチャを引く！'):
-    rarity_probs = {
-        'N': 0.4,
-        'R': 0.3,
-        'SR': 0.2,
-        'SSR': 0.1
-    }
-    chosen_rarity = np.random.choice(list(rarity_probs.keys()), p=list(rarity_probs.values()))
-    subset_df = words_df[words_df['レア度'] == chosen_rarity]
-    selected_word = subset_df.sample().iloc[0]
+    selected_word = words_df.sample().iloc[0]
     
     # セッションステートに選択された単語を保存
     st.session_state.selected_word = selected_word
@@ -35,7 +27,6 @@ if st.button('ガチャを引く！'):
 
 if 'selected_word' in st.session_state:
     st.header(f"単語名: {st.session_state.selected_word['単語']}")
-    st.subheader(f"レア度: {st.session_state.selected_word['レア度']}")
 
     # 意味を確認するボタンを追加
     if st.button('意味を確認する'):
