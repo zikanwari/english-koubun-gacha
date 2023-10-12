f15f8a1844d7ca183f46a04e2e3f5d431c062811
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-10-13 08:42:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,22 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import random
+import time
 
-st.set_page_config(page_title="助動詞ガチャ")
+st.title("ルーレットゲーム")
 
-# タイトルと説明
-st.title('助動詞ガチャ')
+# ルーレットセクションの数
+num_sections = 40
 
-st.write('助動詞をランダムに表示します。')
-st.write('エンドレステスト頑張りましょう！')
+# ルーレットをスタート/ストップするためのボタン
+start_stop_button = st.button("ルーレットをスタート/ストップ")
 
-# Load the data
-@st.cache
-def load_data():
-    return pd.read_excel("助動詞.xlsx")
-
-words_df = load_data()
-
-# ガチャ機能
-if st.button('ガチャを引く！'):
-    selected_word = words_df.sample().iloc[0]
+if start_stop_button:
+    spinning = True
+    result = None
+    while spinning:
+        result = random.randint(1, num_sections)
+        st.write(f"ルーレットがスピン中... 結果: {result}")
+        time.sleep(0.5)
+        spinning = st.button("ルーレットをストップ")
     
-    # セッションステートに選択された単語を保存
-    st.session_state.selected_word = selected_word
-    st.session_state.display_meaning = False
-
-if 'selected_word' in st.session_state:
-    st.header(f"単語名: {st.session_state.selected_word['単語名']}")
-
-    # 意味を確認するボタンを追加
-    if st.button('意味を確認する'):
-        st.session_state.display_meaning = True
-
-    if st.session_state.display_meaning:
-        st.write(f"意味: {st.session_state.selected_word['意味']}")
+    st.write(f"ルーレットが停止しました。最終結果: {result}")
