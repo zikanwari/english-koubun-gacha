449736ac4567fd32857a28a7f4b9ff3b254ac3aa
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-10-13 09:52:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,13 @@ import streamlit as st
 import random
 import time
 
-st.title("ルーレットゲーム")
+st.title("ダイススピンアプリ")
 
-spinning = False
+spinning = st.checkbox("スピン中")
 result = None
 
-# スピン中に表示するテキストエリア
-spin_text = st.empty()
-
-if not spinning:
-    if st.button("スタート"):
-        spinning = True
-        while spinning:
-            result = random.randint(1, 40)
-            spin_text.text(f"ルーレットがスピン中... 結果: {result}")
-            time.sleep(0.5)
-            spinning = not st.button("ストップ")
-        spin_text.text("ルーレットが停止しました。最終結果: {result}")
+if spinning:
+    result = random.randint(1, 40)
+    st.write(f"ルーレットがスピン中... 結果: {result}")
+else:
+    st.write("ルーレットが停止しました。最終結果:", result)
