468fc46cd69fd0fae8db5d53b5f176cc2c7d129f
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-10-13 09:20:42 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,20 @@
 import streamlit as st
 import random
-import time
 
 st.title("ルーレットゲーム")
 
-# ルーレットセクションの数
-num_sections = 40
-
 spinning = False
+result = None
 
-# ルーレットをスタート/ストップするためのボタン
-start_stop_button = st.button("ルーレットをスタート/ストップ", key="start_stop_button")
+if not spinning:
+    if st.button("スタート"):
+        spinning = True
+        result = random.randint(1, 40)
+else:
+    if st.button("ストップ"):
+        spinning = False
 
-if start_stop_button:
-    spinning = True
-    result = None
-    while spinning:
-        result = random.randint(1, num_sections)
-        st.write(f"ルーレットがスピン中... 結果: {result}")
-        time.sleep(0.5)
-        stop_button = st.button("ルーレットをストップ", key="stop_button")
-        spinning = not stop_button
-    
-    st.write(f"ルーレットが停止しました。最終結果: {result}")
+if spinning:
+    st.write(f"ルーレットがスピン中... 結果: {result}")
+else:
+    st.write("ルーレットが停止しました。最終結果: {result}")
