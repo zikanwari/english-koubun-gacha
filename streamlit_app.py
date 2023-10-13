358723a070ce37f03b94385dd3c78c70d73ff095
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-10-13 10:04:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ while 1 == 1:
     # セッションステートに選択された単語を保存
     st.session_state.selected_word = selected_word
     st.session_state.display_meaning = False
-    sleep(1)
+    time.sleep(1)
 
 if 'selected_word' in st.session_state:
     st.header(f"単語名: {st.session_state.selected_word['単語名']}")
