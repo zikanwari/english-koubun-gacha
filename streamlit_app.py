f0acbde58b69a0895a5d6d645771369e3a880d35
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-10-13 09:55:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,6 @@ st.title("ダイススピンアプリ")
 spinning = st.checkbox("スピン中")
 result = None
 
-if spinning:
+while spinning:
     result = random.randint(1, 40)
     st.write(f"ルーレットがスピン中... 結果: {result}")
-else:
-    st.write("ルーレットが停止しました。最終結果:", result)
