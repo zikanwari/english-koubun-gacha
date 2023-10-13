08e18825a8f7cea358c2cd3ebedb2119ab88a404
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-10-13 09:12:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ num_sections = 40
 spinning = False
 
 # ルーレットをスタート/ストップするためのボタン
-start_stop_button = st.button("ルーレットをスタート/ストップ")
+start_stop_button = st.button("ルーレットをスタート/ストップ", key="start_stop_button")
 
 if start_stop_button:
     spinning = True
@@ -19,6 +19,7 @@ if start_stop_button:
         result = random.randint(1, num_sections)
         st.write(f"ルーレットがスピン中... 結果: {result}")
         time.sleep(0.5)
-        spinning = not st.button("ルーレットをストップ")
+        stop_button = st.button("ルーレットをストップ", key="stop_button")
+        spinning = not stop_button
     
     st.write(f"ルーレットが停止しました。最終結果: {result}")
