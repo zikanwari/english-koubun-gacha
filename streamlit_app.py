f026bcda5c3a0a845136eac1b864de1186347e03
launchpencil <66775280+launchpencil@users.noreply.github.com>
2024-09-20 18:40:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,22 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.set_page_config(page_title="助動詞ガチャ")
+st.set_page_config(page_title="英語構文ガチャ")
 
 # タイトルと説明
-st.title('助動詞ガチャ')
+st.title('英語構文ガチャ')
 
-st.write('助動詞をランダムに表示します。')
-st.write('エンドレステスト頑張りましょう！')
+st.write('英語の構文をランダムに表示します。')
 
 # Load the data
 @st.cache
 def load_data():
-    return pd.read_excel("助動詞.xlsx")
+    return pd.read_csv("untitled.xlsx")
 
 words_df = load_data()
 
 # ガチャ機能
-if st.button('ガチャを引く！'):
+if st.button('ガチャを引く！(英→日)'):
     selected_word = words_df.sample().iloc[0]
     
     # セッションステートに選択された単語を保存
@@ -26,11 +25,28 @@ if st.button('ガチャを引く！'):
     st.session_state.display_meaning = False
 
 if 'selected_word' in st.session_state:
-    st.header(f"単語名: {st.session_state.selected_word['単語名']}")
+    st.header(f"英文: {st.session_state.selected_word['english']}")
 
     # 意味を確認するボタンを追加
-    if st.button('意味を確認する'):
+    if st.button('和訳を確認する'):
         st.session_state.display_meaning = True
 
     if st.session_state.display_meaning:
-        st.write(f"意味: {st.session_state.selected_word['意味']}")
+        st.write(f"和訳: {st.session_state.selected_word['japanese']}")
+
+if st.button('ガチャを引く！(日→英)'):
+    selected_word = words_df.sample().iloc[0]
+    
+    # セッションステートに選択された単語を保存
+    st.session_state.selected_word = selected_word
+    st.session_state.display_meaning = False
+
+if 'selected_word' in st.session_state:
+    st.header(f"和文: {st.session_state.selected_word['japanese']}")
+
+    # 意味を確認するボタンを追加
+    if st.button('英訳を確認する'): 
+        st.session_state.display_meaning = True
+
+    if st.session_state.display_meaning:
+        st.write(f"英訳: {st.session_state.selected_word['english']}")
